bfa660f855dd889d369e465192f631f63cfaeddc
JaviOlaya <fullstack@javiolaya.online>
2020-08-26 10:21:04 +0200

    Creacion de la portada

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns =[
     
-    path('', views.index, name="index"),
+    path('', views.portada, name="portada"),
+    path('inicio/', views.index, name="inicio"),
+    path('register/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+# from main_app.forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+def portada(request):
+    
+    return render(request,'mainapp/portada.html',{
+        'title': 'Esta es la portada'
+    })
 
 def index(request):
     
     return render(request,'mainapp/index.html',{
         'title': 'Inicio'
     })
+    
+def register_page(request):
+    
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    else:
+        register_form = RegisterForm()
+        
+        if request.method == 'POST':
+            register_form = RegisterForm(request.POST)
+
+            if register_form.is_valid():
+                register_form.save()
+                messages.success(request, 'te has registrado correctamente')
+                
+                return redirect('index')
+        
+        return render(request, 'users/register.html',{
+            'title': 'Registro',
+            'register_form': register_form
+        })
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    else:
+        if request.method == 'POST':
+            pass
+        else:
+            pass
