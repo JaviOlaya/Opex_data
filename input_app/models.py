dbf6d8698e40b906d537ba54cb0897b449748882
JaviOlaya <fullstack@javiolaya.online>
2020-10-09 01:00:00 +0200

    integracion de vistas en el programa

--- input_app/models.py
+++ input_app/models.py
@@ -2,31 +2,72 @@ from django.db import models
 from django.db.models import Model 
 from django.conf import settings
 # Create your models here.
-
 # Create your models here.
+class Proyecto(models.Model):
+    Nombre_proyecto  = models.CharField(max_length=50)
+    Fecha_de_inicio = models.DateField()
+
+    def DatosProyecto(self):
+        cadena = "Nombre:{0}"
+        return cadena.format(self.Nombre_proyecto,self.Fecha_de_inicio)
+    
+    def __str__ (self):
+        return self.DatosProyecto()
+        
+        
 class Personal(models.Model):
+    DEPARTAMENTO_CHOICES = (
+        ('0', 'Gerente'),
+        ('1', 'Comercial'),
+        ('2', 'Administrativo'),
+        ('3', 'Soporte')
+    )
+    # PrimerApellido = models.CharField(max_length=50)
+    # SegundoApellido = models.CharField(max_length=50)
+    # Nombres= models.CharField(max_length=50)
+    # DNI = models.CharField(max_length=8)
     FondoFijo = models.BooleanField(default='true')
+    # FechaNacimiento = models.DateField()
+    # Sexos=(('F','Femenino'),('M','Masculino'))
+    # Sexo = models.CharField(max_length=1, choices=Sexos,default='M')
     Num_empleados =models.PositiveIntegerField(default='0')
-    Plantillas = (('Gerente','Gerente'), ('Comercial','Comercial'),('Administrativo','Administrativo'),('Soporte','Soporte'))
-    Plantilla = models.CharField(max_length=20,choices=Plantillas,default='S')
+    Plantillas = models.CharField(
+        'Departamento',
+        max_length=30,
+        choices= DEPARTAMENTO_CHOICES,
+        default='0', 
+    )
+  
     Sueldo = models.FloatField(default='0')
     
     def DatosPersonal(self):
         cadena ="Datos de empleados:{0}"
         # return cadena.format(self.PrimerApellido,self.SegundoApellido,self.Nombres)
-        return cadena.format(self.Plantilla,self.Num_empleados)
+        return cadena.format(self.Plantillas,self.Num_empleados)
    
     def __str__ (self):
         return self.DatosPersonal()
 
 
 class GastosAdministrativos(models.Model):
+    GADMIN_CHOICES = (
+        ('0', 'Consulta Agricola'),
+        ('1', 'Formación'),
+        ('2', 'Entrenamiento'),
+        ('3', 'POL en necesidades administrativas'),
+        ('4', 'Gastos de Oficina'),
+        ('5', 'Gastos de Entretenimiento'),
+        ('6', 'Gastos de Viajes'),
+        ('7', 'Servicios de Información'),
+        ('8', 'Gasto seguro de propiedad'),
+        
+    )
     Nombre =models.CharField(max_length=50)
     Descripcion = models.TextField(max_length=200,blank=True)
     FechaGasto=models.DateField()
     Cantidad= models.FloatField(default='0')
-    Tipo = (('Consulta Agricola','Agronomy_consulting'),('Formación','Formacion'),('Entrenamiento','Entrenamiento'),('Necesidades administrativas','POL on administrative needs'),('Gastos de Oficina','Office_expenses'),('Gastos de Entretenimiento','Entertainment_expenses'),('Gastos de Viajes','Travel_expenses'),('Servicios de Información','Information_services'),('Gasto seguro de propiedad','Property_Insurance'))
-    TipoGasto = models.CharField(max_length=30,choices=Tipo,default='Consulta Agricola')
+   
+    TipoGasto = models.CharField(max_length=1,choices=GADMIN_CHOICES,default='0')
     
     def DatosGastosAdministrativos(self):
         cadena = "{0},{1}"
@@ -36,10 +77,11 @@ class GastosAdministrativos(models.Model):
         return self.DatosGastosAdministrativos()
 
 class GastosCultivos(models.Model):
+
     UNIT_CHOICES = (
-        ('Kilogramos', 'Kilogramos'),
-        ('Litros', 'Litros'),
-        ('Unidades', 'Unidades'),
+        ('0', 'Kilogramos'),
+        ('1', 'Litros'),
+        ('2', 'Unidades'),
     )
 
     FechaActualizacion= models.DateField()
@@ -85,8 +127,8 @@ class GastosCultivos(models.Model):
         choices=UNIT_CHOICES,
         default='Kilogramos',
     )
-    SemillasPrecio = models.DecimalField(max_digits = 7, decimal_places = 2)
-    CantidadSemillas = models.DecimalField(max_digits = 7, decimal_places = 2)
+    SemillasPrecio = models.DecimalField(max_digits=7, decimal_places=2)
+    CantidadSemillas = models.DecimalField(max_digits=7, decimal_places=2)
     unitSemillas = models.CharField(
         'unidad de medida',
         max_length=30,
@@ -94,8 +136,8 @@ class GastosCultivos(models.Model):
         default='Kilogramos', 
     )
     
-    CombustiblePrecio = models.DecimalField(max_digits = 7, decimal_places = 2)
-    CantidadCombustible = models.DecimalField(max_digits = 7 , decimal_places=2)
+    CombustiblePrecio = models.DecimalField(max_digits=7, decimal_places=2)
+    CantidadCombustible = models.DecimalField(max_digits=7, decimal_places=2)
     unitCombustible = models.CharField(
         'unidad de medida',
         max_length=30,
@@ -125,19 +167,6 @@ class GastosCultivos(models.Model):
     def __str__ (self):
         return self.GastosCultivosTotal()
     
-class CapitalTrabajo(models.Model):
-    CapitalStock = models.DecimalField(max_digits=7, decimal_places=2)
-    monedaStock = models.CharField(max_length=1,default='€')
-    CapitalDeudores = models.DecimalField(max_digits=7, decimal_places=2)
-    monedaDeudores = models.CharField(max_length=1,default='€')
-    CapitalAcreedores = models.DecimalField(max_digits=7, decimal_places=2)
-    monedaAcreedores = models.CharField(max_length=1,default='€')
-
-    def CapitalTrabajoTotal(self):
-        cadena = " {0}"
-        return cadena.format(self.CapitalStock)
-
-    def __str__ (self):
-        return self.CapitalTrabajoTotal()
+# class CapitalTrabajo(models.Model):
     
-    
+
--- input_app/views.py
+++ input_app/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.views.generic import(
+    ListView,
+    DetailView,
+    CreateView
+)
+from .models import Personal
+from .models import GastosAdministrativos
+from .models import GastosCultivos
 
-# Create your views here.
+class ListaAllEmpleados(ListView):
+    template_name="empleados/listaEmpleados.html"
+    #La paginación se hace muy fácil
+    paginate_by = 1
+    model = Personal
+    context_object_name = 'listaEmpleados'
+    
+class ListByArea(ListView):
+    template_name="empleados/listaEmpleados_by_area.html"
+    # queryset = Personal.objects.filter(
+    #     Plantilla = 'Soporte'
+    # )
+    #Funcion para escribir filtro en un listado por metodo get
+    def get_queryset(self):
+        
+        area = self.kwargs['Plantilla']
+        lista = Personal.objects.filter(
+        Plantilla = area
+        )
+        return lista
+    
+#4Listar los empleados por palabra clave
+class ListaEmpleadosByKword(ListView):
+    template_name = "empleados/Empleados_kword.html"
+    context_object_name ="empleados"
+    
+    def get_queryset(self):
+        print("*******************************")
+        palabra_clave = self.request.GET.get('kword','')
+        print('=======',palabra_clave)
+        lista = Personal.objects.filter(
+            Plantilla = palabra_clave
+        )
+        print('lista resultado: ',lista)
+        return lista
+    
+class IntGastosAdmin(CreateView):
+    template_name = 'Gastos/intGastoAdmin.html'
+    model=GastosAdministrativos
+    fields=('__all__')
+    #web a la que tiene que redirigirse al guardar los datos del formulario
+    succes_url ="."
+    # context_object_name = "Gastos Administrativos"
+class IntGastosCultivos(CreateView):
+    template_name = 'Gastos/intGastosCultivos.html'
+    model=GastosCultivos
+    fields=('__all__')
+    #web a la que tiene que redirigirse al guardar los datos del formulario
+    succes_url ="."    
